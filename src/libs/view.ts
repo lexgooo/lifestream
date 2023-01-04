49334f2a81085245a9113a314ad061acded3d882
lexgooo <beyondc@foxmail.com>
2023-01-04 16:10:44 +0800

    update: json页面渲染为表格功能实现

--- src/libs/view.ts
+++ src/libs/view.ts
@@ -1,23 +1,68 @@
 import { TextFileView } from "obsidian";
+import { TableColumn, Result } from "./config";
 
 export const VIEW_TYPE_TABLE_JSON = 'table-json-view'
 
 export class TableJsonView extends TextFileView {
+  tableColumns: TableColumn[]
+  tableData: Result[]
+
+  tableEl: HTMLElement
+
   getViewData () {
-    return this.data;
+    return JSON.stringify({
+      columns: this.tableColumns,
+      data: this.tableData
+    })
   }
 
-  setViewData (data: string, clear: boolean) {
-    this.data = data;
-    this.contentEl.empty();
-    this.contentEl.createDiv({ text: this.data });
+  setViewData (res: string, clear: boolean) {
+    this.data = res;
+    const { columns, data } = JSON.parse(res)
+    if (!(Array.isArray(columns) && Array.isArray(data))) {
+      // TODO 渲染为 json 格式化显示文件
+      return
+    }
+    this.tableColumns = columns
+    this.tableData = data
+    this.refresh()
   }
 
   clear () {
-    this.data = "";
+    this.tableColumns = [];
+    this.tableData = [];
   }
 
   getViewType () {
     return VIEW_TYPE_TABLE_JSON
+  }
+
+  async onOpen () {
+    const boxEl = this.contentEl.createDiv({ cls: 'markdown-rendered' })
+    this.tableEl = boxEl.createEl('table');
+  }
+
+  async onClose () {
+    this.contentEl.empty();
+  }
+
+  refresh () {
+    this.tableEl.empty();
+
+    const headEl = this.tableEl.createEl('thead')
+    const bodyEl = this.tableEl.createEl('tbody')
+
+    const headTr = headEl.createEl('tr')
+    this.tableColumns.forEach((column, i) => {
+      headTr.createEl('th', { text: column.title })
+    })
+
+    this.tableData.forEach((item, i) => {
+      const tr = bodyEl.createEl('tr')
+      this.tableColumns.forEach((column, i) => {
+        if (!Object.keys(item).includes(column.key)) return
+        tr.createEl('td', { text: item[column.key]+'' })
+      })
+    })
   }
 }
